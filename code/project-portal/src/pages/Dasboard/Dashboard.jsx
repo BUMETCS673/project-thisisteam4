44e3418180a4507dbbaa1d303eefe9c7cfc75caa
brookelove <sblove@bu.edu>
2024-05-28 23:26:36 -0400

    adding in the bones of the card, dashboard, and searchbar

--- code/project-portal/src/pages/Dasboard/Dashboard.jsx
+++ code/project-portal/src/pages/Dasboard/Dashboard.jsx
@@ -1,9 +1,23 @@
+import ProjectCard from "../../components/ProjectCard/ProjectCard";
+import SearchBar from "../../components/SearchBar/SearchBar";
 import "./Dashboard.css"
 const Dashboard= () => {
+    let cards = [];
+    let renderProjectCards = () => {
+        for(let i = 0; i < 5; i++){
+            cards.push(<ProjectCard key={i} />);
+
+        }
+    }
+    renderProjectCards()
     return(
         <main>
-        <h1>Dasboard Route</h1>
-        <a>Go Back to Home</a>
+            <SearchBar/>
+            {/* total amount of projects on page */}
+            <h2>({cards.length}) Projects</h2>
+            <section className="projects-container">
+                {cards}
+            </section>
         </main>
     )
 } 
