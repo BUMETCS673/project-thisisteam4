5102a7d191b375f772a13af1be48682938e1acbc
fbaltaci <fbaltaci@advisor360.com>
2024-06-02 21:08:56 -0400

    Update Footer.jsx

--- code/project-portal/src/components/Footer/Footer.jsx
+++ code/project-portal/src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 // Footer.js
-import React from 'react';
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
   return (
@@ -9,6 +9,9 @@ const Footer = () => {
         <p>&copy; 2024 Project-Portal. All rights reserved.</p>
         <div className="footer-links">
           <a href="/about">About</a>
+          {/* <a href="/terms">Terms of Service</a>
+          <a href="/privacy">Privacy Policy</a>
+          <a href="/contact">Contact Us</a> */}
         </div>
       </div>
     </footer>
