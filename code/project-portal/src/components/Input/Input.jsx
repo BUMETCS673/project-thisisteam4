6490f54ca84860bb8f95da6c93c367062cf88219
Natanim Issa <natanimissa@gmail.com>
2024-06-09 22:09:44 -0400

    added the password show icon

--- code/project-portal/src/components/Input/Input.jsx
+++ code/project-portal/src/components/Input/Input.jsx
@@ -1,20 +1,40 @@
 import "./Input.css";
 import { useField } from "formik";
+import { useState } from "react";
+import { FiEyeOff, FiEye } from 'react-icons/fi';
 
 function Input({ label = "User Input", ...props }) {
   const [field, meta] = useField(props);
+  const [show, setShow] = useState(false);
 
   return (
     <div className="inputContainer">
       <label className="label" htmlFor={props.id}>
         {label}
       </label>
-      <input
-        {...props}
-        {...field}
-        className={meta.error && meta.touched ? "input input-error" : "input"}
-        autoComplete="true"
-      />
+      {props.type !== "password" && (
+        <input
+          {...props}
+          {...field}
+          className={meta.error && meta.touched ? "input input-error" : "input"}
+          autoComplete="true"
+        />
+      )}
+      {props.type === "password" && (
+        <input
+          {...props}
+          {...field}
+          type={show ? "text" : "password"}
+          className={meta.error && meta.touched ? "input input-error" : "input"}
+          autoComplete="true"
+        />
+      )}
+      {props.type === "password" && (
+        <p onClick={() => setShow(!show)} className="show-pass-icon">
+          {show && <FiEye />}
+          {!show && <FiEyeOff />}
+        </p>
+      )}
       {meta.error && meta.touched && <p className="error">{meta.error}</p>}
     </div>
   );
